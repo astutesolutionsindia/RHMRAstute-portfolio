6ecb2a29233d5112c9dbcc5eb715019bea5e9cee
Raashid <pcraashid@gmail.com>
2026-05-11 23:22:41 +0530

    test: remove all overflow rules and disable bubbles

--- src/app/components/RootLayout.tsx
+++ src/app/components/RootLayout.tsx
@@ -1,23 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import { Navigation } from './Navigation';
-import { FloatingBubbles } from './FloatingBubbles';
-import { CursorFollower } from './CursorFollower';
-import { Footer } from './Footer';
-
 export function RootLayout() {
   return (
-    /* 1. REMOVED the bg-gradient classes (we will put them in CSS).
-       2. REMOVED overflow-x-hidden (it causes the mobile scroll lock bug). */
     <div className="relative flex flex-col min-h-screen w-full">
+      {/* 🛑 COMMENT THESE OUT FOR THIS TEST */}
+      {/* <FloatingBubbles /> */}
+      {/* <CursorFollower /> */}
       
-      <FloatingBubbles />
-      <CursorFollower />
       <Navigation />
-      
       <main className="relative z-10 flex-grow">
         <Outlet />
       </main>
-      
       <Footer />
     </div>
   );
